2e65a94fadbe6c1ad650da724bf85cd59bdc5d73
Gabriel Duarte <gabrielqduarte@gmail.com>
2024-04-17 15:44:49 +0200

    increase playwright timeouts

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -4,6 +4,29 @@ import { toMatchImageSnapshot } from "jest-image-snapshot";
 const customSnapshotsDir = `${process.cwd()}/__snapshots__`;
 
 const config: TestRunnerConfig = {
+  async prepare({ page, browserContext, testRunnerConfig }) {
+    page.setDefaultTimeout(5 * 60 * 1000);
+
+    const targetURL = process.env.TARGET_URL;
+    const iframeURL = new URL("iframe.html", targetURL).toString();
+
+    if (testRunnerConfig.getHttpHeaders) {
+      const headers = await testRunnerConfig.getHttpHeaders(iframeURL);
+      await browserContext.setExtraHTTPHeaders(headers);
+    }
+
+    await page.goto(iframeURL, { waitUntil: "load" }).catch((err) => {
+      if (
+        err instanceof Error &&
+        err.message.includes("ERR_CONNECTION_REFUSED")
+      ) {
+        const errorMessage = `Could not access the Storybook instance at ${targetURL}. Are you sure it's running?\n\n${err.message}`;
+        throw new Error(errorMessage);
+      }
+
+      throw err;
+    });
+  },
   setup() {
     expect.extend({ toMatchImageSnapshot });
   },
